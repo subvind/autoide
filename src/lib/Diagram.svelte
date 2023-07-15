64b24e20c77f08c17f76bd39d55c5dc64b821213
c_u_l8er_true ucimecu inomauto <subvind@uni.lan>
2023-07-15 12:39:56 -0500

    greet and diagram

---
+++ src/lib/Diagram.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import { Svelvet, Node, Anchor, Edge, Controls } from 'svelvet';
+
+  export let width: number;
+  export let height: number;
+</script>
+
+<Svelvet width={width} height={height - 300} theme="dark" initialZoom={0.6} minimap>
+	<Node />
+	<Node label="Demo Node" inputs={3} />
+	<Node id="node-id" inputs={2} />
+	<Node bgColor="red" inputs={10} outputs={5} height={200} position={{ x: 100, y: 100 }} />
+	<Controls slot="controls" horizontal />
+</Svelvet>
+test
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,15 @@
+<script lang="ts">
+  import Greet from '../lib/Greet.svelte'
+  import Diagram from '../lib/Diagram.svelte'
+
+  let height: number = 0;
+  let width: number = 0;
+</script>
+
+<svelte:window bind:innerHeight={height} bind:innerWidth={width} />
+
+<Diagram height={height} {width} />
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentations</p>
+<Greet />
